7e86fba0de26060ead00a4565acca9a7eb6773bd
Muhammad Nauman Raza <devraza@outlook.com>
2020-12-12 09:28:07 +0000

    Made The Bot Run.

---
+++ lib/bot/__init__.py
@@ -0,0 +1,49 @@
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from discord import VersionInfo
+from discord.ext.commands import Bot as BotBase
+
+PREFIX = "::"
+OWNER_IDS = [549213551236087808]
+
+class Bot(BotBase):
+    def __init__(self):
+        self.PREFIX = PREFIX
+        self.ready = False
+        self.guild = None
+        self.scheduler = AsyncIOScheduler()
+
+        super().__init__(command_prefix=PREFIX, owner_ids=OWNER_IDS)
+
+    def run(self, version):
+        self.VERSION = version
+
+        with open("./lib/bot/token.0", "r", encoding="utf-8") as tf:
+            self.TOKEN = tf.read()
+
+        print("Running Bot...")
+        super().run(self.TOKEN, reconnect=True)
+
+    async def on_connect(self):
+        print("Bot Connected.")
+
+    async def on_disconnect(self):
+        print("Bot Disconnected.")
+
+    async def on_ready(self):
+        if not self.ready:
+            self.ready = True
+            print("Bot Ready.")
+
+        else:
+            print("Bot Reconnected.")
+
+
+    async def on_message(self, message):
+        pass
+
+bot = Bot()
+         
+
+    
+
+        
